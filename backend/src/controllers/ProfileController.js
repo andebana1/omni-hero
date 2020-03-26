faa1d646a2a75613805520b21abf6422c21fe3fd
andebana1 <andersonvieira14@gmail.com>
2020-03-26 11:25:58 -0300

    Front-end do app

--- backend/src/controllers/ProfileController.js
+++ backend/src/controllers/ProfileController.js
@@ -1,9 +1,10 @@
 const connection = require('../database/connection');
 
 module.exports = {
-    async index(response, request){
+    async index(request, response){
+        console.log("Aqui");
         const ong_id = request.headers.authorization;
-
+        console.log(ong_id);
         const incidents = await connection('incidents').where('ong_id', ong_id).select('*');
 
         return response.json(incidents);
